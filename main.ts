0cc0e32ac0ec31740889d77944b5a48326b2c8e5
trakturektomas <128743212+trakturektomas@users.noreply.github.com>
2023-03-25 18:19:19 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+let časkůasdkfj = 0
+let atickkkkooooooo = 1000
+let výška = 1000
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        výška,
+        výška + 100,
+        atickkkkooooooo,
+        atickkkkooooooo + 450,
+        časkůasdkfj,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Logarithmic
+        ), SoundExpressionPlayMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        časkůasdkfj = 1000000
+    } else {
+    	
+    }
+})
